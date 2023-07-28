ac16806252beaad20e14f4be66e6b2bc05187694
lilchad078 <lilchad08@gmail.com>
2023-07-28 10:34:13 +0000

    ubah index

---
+++ MoodTer-main/frontendUas/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {  RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <div class="flex my-8 justify-center">
+      <RouterView />
+    </div>
+  </header>
+
+</template>
